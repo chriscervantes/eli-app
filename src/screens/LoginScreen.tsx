a7b7589a00ef1c488d545627b328e4875d33b41a
Christopher Alphonse Cervantes <cervantes.alphonse@yahoo.com>
2024-09-07 19:34:28 +1000

    minor changes, update the login screen with password field

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -3,11 +3,19 @@ import { useState } from 'react'
 import { Alert, StyleSheet, View, Text, TextInput, Button } from 'react-native'
 
 const LoginScreen = ({ navigation }: any) => {
-  const [username, setUserName] = useState('')
+  const [username, setUserName] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
 
   const handleLogin = async () => {
     if (username.trim() === '') return
     try {
+      /**
+       * 1. check if user exists in the database or identity pool (cognito)
+       * 2. if user exists, save user data to AsyncStorage and give the use the access token which might be valid after 15-20 mins of inactive.
+       * 3. store the last state before kick the user out from the app.
+       * 4. force the user to logout after 15-20 mins of inactive
+       */
+
       await AsyncStorage.setItem('user', username)
       navigation.replace('Home')
     } catch (error) {
@@ -17,12 +25,18 @@ const LoginScreen = ({ navigation }: any) => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
+      <Text style={styles.title}>Login </Text>
       <TextInput
         style={styles.input}
         placeholder="Enter your username"
         value={username}
         onChangeText={setUserName}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your password"
+        value={password}
+        onChangeText={setPassword}
       />
       <Button title="Login" onPress={handleLogin} />
     </View>
